a6790525d1209a2857478453cf8daf63664333fc
Kevin Terpstra <kevinterpstra@MBP-van-Kevin.home>
2020-08-06 10:24:09 +0200

    Order confirmed detail

--- resources/views/order-confirmed.blade.php
+++ resources/views/order-confirmed.blade.php
@@ -12,7 +12,7 @@
                 <div class="card shadow-lg">
                   <!-- <h1><i class="fa fa-check color-main"></i></h1> -->
                   <div class="card-body">
-                    <h4>Hey {{ Auth::user()->first_name }},</h4>,
+                    <h4>Hey {{ Auth::user()->first_name }},</h4>
                     <h1>{{__('cart.order_confirmed')}}</h1>
                     <p>{{__('cart.order_confirmed_sub')}}</p>
                     <a href="{{route('order-details', session('last_created_order_id'))}}" class="btn btn-nw">{{__('cart.show_order_details')}}</a>
